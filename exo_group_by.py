5a35480a71ce6e925c77fba36be371247d601344
mlamamra <mlamamra@dawan.fr>
2022-07-19 16:27:54 +0200

    exo titanic

---
+++ exo_group_by.py
@@ -0,0 +1,6 @@
+# exo: à partir de l'url suivante: 
+# http://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv
+# analyser les données
+# calculer le pourcentage de survivants chez les femmes et les enfants par classe
+# hint: le groupby est une opération couteuse en ressources
+# il est donc préférable de se donner un dataframe réduit aux données utiles en premier lieu
