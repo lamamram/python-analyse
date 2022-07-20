ed254a59412e993b159baae928a30f4aef23544d
mlamamra <mlamamra@dawan.fr>
2022-07-20 15:24:06 +0200

    exo ecriture sql

--- cours_pandas_sql.py
+++ cours_pandas_sql.py
@@ -36,3 +36,16 @@ pays_df = pd.read_sql("pays", df_conn, index_col="iso2")
 pays_df = pd.read_sql("SELECT * FROM pays WHERE name LIKE 'F%'", df_conn, index_col="iso2")
 pays_df
 # %%
+# insertion d'un csv dans une table en passant par pandas
+url = "http://www.afnic.fr/wp-media/ftp/documentsOpenData/202105_OPENDATA_A-NomsDeDomaineEnPointFr.zip"
+
+## télécharger les données
+# 1. se donner un dataframe avec l'url
+# 2. écrire le df en local dans un .zip
+# 3. ne faire ceci que si ce n'est pas déjà fait
+
+
+## insérer les données du df dans la table domain_name
+# 4. mettre en cohérence les noms des colonnes utiles
+# 5. écrire le to_csv sans modifier le schéma de la table
+# 6. écrire le to_csv de façon à créer (ou écraser) le schéma à partir du df
