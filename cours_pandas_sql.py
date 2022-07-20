cb5c7968cc9e6ca6ea8481599ffb61c747fe98b1
mlamamra <mlamamra@dawan.fr>
2022-07-20 15:03:25 +0200

    rappel contexts

--- cours_pandas_sql.py
+++ cours_pandas_sql.py
@@ -14,3 +14,18 @@ from sqlalchemy.types import Integer, String, Text
 print(pd.__version__)
 print(np.__version__)
 # %%
+# création de la base de données
+try:
+    # connexion db
+    with sqlite3.connect("dns.db") as conn:
+        # création d'un prompt pout exécuter les requêtes
+        cur = conn.cursor()
+        with open("domain_name_sqlite3.sql") as f:
+            script = f.read()
+            cur.executescript(script)
+except (sqlite3.OperationalError, sqlite3.DatabaseError) as e:
+    print(e)
+except ConnectionError as ce:
+    print(ce)
+
+# %%
