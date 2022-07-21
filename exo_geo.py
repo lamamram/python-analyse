9891801256c88dff021cf90139da002377dd712d
mlamamra <mlamamra@dawan.fr>
2022-07-21 10:41:48 +0200

    nettoyage de dataframes

--- exo_geo.py
+++ exo_geo.py
@@ -16,11 +16,26 @@ dns_df
 # 3. transformer le df pour obtenir 3 colonnes
 # pays, ville et nombre de dns par ville
 # Hint: regarder la fonction value_counts des dataframes
-
-
+s = dns_df.groupby(["Pays BE", "Ville BE"]).value_counts()
+s.name = "nb"
+dns_df = s.reset_index()
+dns_df
+# %%
 # 4. ouvrir le df geo_dns et nettoyer la colonne loc pour la mettre
 # en cohérence avec la colonne Ville BE
 # Hint: utiliser les fonction .str.replace et .str.split des séries de type object
+geo_df = pd.read_csv(
+    "geo_dns.csv",
+    encoding="utf8",
+    index_col=0
+)
+geo_df["loc"] = geo_df["loc"].str.replace(
+    ", ([A-Z]{2})$",
+    repl=lambda m: "|" + m.group(1),
+    regex=True
+)
+geo_df[["loc", "iso2"]] = geo_df["loc"].str.split("|", expand=True)
+geo_df
 
 # 5. joindre les deux dataframes pour obtenir 5 colonnes avec lat, lon
 
